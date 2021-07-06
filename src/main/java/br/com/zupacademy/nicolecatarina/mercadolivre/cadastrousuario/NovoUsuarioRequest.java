2de5c72b44c3b8f51a11bae13feadfa875987614
Nicole Catarina <nicole.silva@zup.com.br>
2021-07-06 14:19:32 -0300

    1-1-1-nao-pode-ter-usuario-mesmo-email

--- src/main/java/br/com/zupacademy/nicolecatarina/mercadolivre/cadastrousuario/NovoUsuarioRequest.java
+++ src/main/java/br/com/zupacademy/nicolecatarina/mercadolivre/cadastrousuario/NovoUsuarioRequest.java
@@ -1,5 +1,6 @@
 package br.com.zupacademy.nicolecatarina.mercadolivre.cadastrousuario;
 
+import br.com.zupacademy.nicolecatarina.mercadolivre.validation.annotation.UniqueValue;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
@@ -9,6 +10,7 @@ public class NovoUsuarioRequest {
 
     @Email
     @NotBlank
+    @UniqueValue(domainClass = Usuario.class, fieldName = "email")
     private String email;
     @NotBlank
     @Length(min = 6)
